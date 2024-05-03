0b313dee125820c240d5c3135870e58983d597cf
Jose Martins <jose.martins@inm.pt>
2024-05-03 18:42:59 +0100

    fix room dependencies

--- projeto_2/app/build.gradle.kts
+++ projeto_2/app/build.gradle.kts
@@ -98,4 +98,7 @@ dependencies {
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    implementation("androidx.room:room-ktx:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
 }
